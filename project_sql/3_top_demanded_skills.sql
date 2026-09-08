bea90d9c7673b26abb05285d5726d2b550297f1f
Michael Artagnan O. Urmanita <152667545+artxnian@users.noreply.github.com>
2026-09-08 09:31:22 +0800

    Add SQL script to analyze top demanded skills for data analysts

---
+++ project_sql/3_top_demanded_skills.sql
@@ -0,0 +1,21 @@
+/*
+What are the most in-demand skills for data analysts?
+- Join job postings to inner join table similar to query 2
+- Identify the top 5 in-demand skills for a data analyst
+- Focus on all job postings.
+- Why? Retrieves the top 5 skills with the highest demand in the job market,
+    providing insights into the most valuable skills for job seekers.
+*/
+
+SELECT
+    skills.skills,
+    COUNT(*) AS number_of_jobs
+FROM
+    job_postings_fact jobs
+INNER JOIN skills_job_dim skills_job ON jobs.job_id = skills_job.job_id
+INNER JOIN skills_dim skills ON skills_job.skill_id = skills.skill_id
+GROUP BY
+    skills.skills
+ORDER BY
+    number_of_jobs DESC
+LIMIT 5;
